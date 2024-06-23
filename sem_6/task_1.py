292b98a33da789c68eeff1f38247ad1049bb3c5a
zoya-ivanova <116255881+zoya-ivanova@users.noreply.github.com>
2024-06-23 22:17:40 +0300

    Delete sem_6 directory

--- sem_6/task_1.py
+++
@@ -1,8 +0,0 @@
-# В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку.
-
-from module_task_1 import check_date, check_year
-
-
-if __name__ == '__main__':
-    print(check_date('30.02.2005'))
-    print(check_year(2000))
